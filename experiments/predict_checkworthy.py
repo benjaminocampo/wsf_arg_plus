89c458b50c1a8c3c4327a0d419ec6c605058dced
Nicolás-Benjamín Ocampo <nicola@gup-131-129.cwi.nl>
2025-09-17 09:22:39 +0200

    Add experiment scripts

---
+++ experiments/predict_checkworthy.py
@@ -0,0 +1,98 @@
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from dotenv import load_dotenv
+from utils import flatten_dict
+from huggingface_hub import login
+from omegaconf import DictConfig, OmegaConf
+
+from vllm import LLM, SamplingParams
+from vllm.sampling_params import GuidedDecodingParams
+
+import hydra
+import logging
+import pandas as pd
+import shlex
+import sys
+import mlflow
+import os
+
+load_dotenv()
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+login(HF_TOKEN)
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+def run_experiment(cfg: DictConfig, run: mlflow.ActiveRun):
+    """
+    Use LLMs to generate the labels for the Check Worthyness task. That is,
+    verify if a message can be considered relevant to be fact-checked.
+    """
+    output_dir = Path(cfg.input.output_dir)
+
+    logger.info("Command-line Arguments:")
+    logger.info(f"Raw command-line arguments: {' '.join(map(shlex.quote, sys.argv))}")
+
+    df = pd.read_parquet(cfg.input.data_path)
+
+    llm = LLM(model=cfg.llm.params.model, tensor_parallel_size=cfg.llm.params.tensor_parallel_size)
+    guided_decoding_params = GuidedDecodingParams(choice=cfg.prompt.output_labels)
+    sampling_params = SamplingParams(
+        guided_decoding=guided_decoding_params,
+        max_tokens=cfg.llm.params.max_tokens,
+        #temperature=cfg.llm.params.temperature,
+    )
+
+    logger.info("generate labels...")
+
+    arg_cols = ["premise0", "premise1", "premise2", "conclusion"]
+    for col in arg_cols:
+        df[f"{col}_prompt_check_worthy"] = df[col].apply(
+            lambda t: [
+                {"role": "system", "content": cfg.prompt.system},
+                {"role": "user", "content": cfg.prompt.user.format(input_text=t)},
+            ]
+        )
+
+        outputs = llm.chat(
+            prompts=df[f"{col}_prompt_check_worthy"].tolist(),
+            sampling_params=sampling_params,
+        )
+        df[f"{col}_gen_check_worthy"] = outputs
+
+    df.to_csv(output_dir / f"{cfg.llm.name}_check_worthy_gens.csv", index=False)
+    mlflow.log_artifact(output_dir)
+
+
+@hydra.main(config_path="conf", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    OmegaConf.register_new_resolver("eval", lambda x: eval(x))
+
+    if cfg.input.uri_path is not None:
+        mlflow.set_tracking_uri(cfg.input.uri_path)
+        assert cfg.input.uri_path == mlflow.get_tracking_uri()
+
+    logger.info(f"Current tracking uri: {cfg.input.uri_path}")
+
+    mlflow.set_experiment(cfg.input.experiment_name)
+    mlflow.set_experiment_tag("mlflow.note.content", cfg.input.experiment_description)
+
+    with mlflow.start_run(run_name=cfg.input.run_name) as run:
+        logger.info("Logging configuration as artifact")
+        with TemporaryDirectory() as tmpdir:
+            config_path = Path(tmpdir) / "config.yaml"
+            with open(config_path, "wt") as fh:
+                print(OmegaConf.to_yaml(cfg, resolve=False), file=fh)
+            mlflow.log_artifact(config_path)
+
+        logger.info("Logging configuration parameters")
+        # Log params expects a flatten dictionary, since the configuration has
+        # nested configurations (e.g. train.model), we need to use flatten_dict
+        # in order to transform it into something that can be easilty logged by
+        # MLFlow.
+        mlflow.log_params(flatten_dict(OmegaConf.to_container(cfg, resolve=False)))
+        run_experiment(cfg, run)
