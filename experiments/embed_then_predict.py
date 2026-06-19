6ff70b61310529163232088854369a51e1b0786a
Nicolás Benjamín Ocampo <nicolasbenjaminocampo@gmail.com>
2026-06-19 16:08:39 +0200

    Fix typo input.run_name instead of run_name

--- experiments/embed_then_predict.py
+++ experiments/embed_then_predict.py
@@ -150,8 +150,8 @@ def run_experiment(cfg: DictConfig, run: mlflow.ActiveRun):
                 results.append(res)
 
     df_results = pd.DataFrame(results)
-    df_results.to_csv(f"{cfg.experiment.path_results}/{cfg.run_name}.csv", index=False)
-    mlflow.log_artifact(f"{cfg.experiment.path_results}/{cfg.run_name}.csv")
+    df_results.to_csv(f"{cfg.experiment.path_results}/{cfg.input.run_name}.csv", index=False)
+    mlflow.log_artifact(f"{cfg.experiment.path_results}/{cfg.input.run_name}.csv")
 
 
 @hydra.main(config_path="conf", config_name="config", version_base=None)
